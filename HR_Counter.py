a9b04f7f7d9968e505cb9bd32396ec2e325ef126
Stuti Mishra <43439858+stutimishra28@users.noreply.github.com>
2021-03-22 00:22:39 +0530

    Update HR_Counter.py
    This is solution to a hackerrank problem

--- HR_Counter.py
+++ HR_Counter.py
@@ -1,12 +1,12 @@
 from collections import Counter
 n=int(input())
-shoes=list(map(int,input().strip().split()))[:n]
-ssc=Counter(shoes)
+shoes=list(map(int,input().strip().split()))[:n] #list of shoe_sizes input
+ssc=Counter(shoes) #convert list to dict of counter
 customers=int(input())
-m=0
+money=0
 for _ in range(customers):
     size, price=map(int,input().split())
     if ssc[size]:
-        m=m+price
+        money=money+price
         ssc[size]-=1
-print(m)       
+print(money)       
